2edc4a4ec22e57dac210088abfa2ccb68e5b9af0
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2018-08-05 05:11:09 +0900

    args 追加

--- Atari_Imitation/Atari_Imitation.py
+++ Atari_Imitation/Atari_Imitation.py
@@ -1,5 +1,6 @@
 import os
 import time
+import argparse
 
 from PIL import Image
 import numpy as np
@@ -20,7 +21,7 @@ from gym import wrappers
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # 軌跡データフォルダ
-PATH = "/Users/SHIGAYUYA/Desktop/atari_v2_release"
+PATH = None
 
 # 環境名
 GAME_NAME = "qbert"
@@ -40,7 +41,10 @@ EPOCHS = 15
 USE_TRAJ_RATIO = 0.01
 
 # 前処理実行
-RAN_PREPROCESS = False
+RUN_PREPROCESS = False
+
+# ラベルごとの重み適用
+USE_CLASS_WEIGHT = False
 
 # Action変換表
 """
@@ -51,7 +55,8 @@ Qbertのaction
 """
 act_trans_list = (0,1,2,3,4,5,2,2,3,4,2,3,4,5,2,2,3,4)
 
-def load_traj_prepro(nb_action, p=0.05, concat=True):
+
+def load_traj_prepro(nb_action, p=0.01, concat=True):
     """行動の軌跡の上位pを取得し、前処理"""
     traj_score = []
     for traj in os.listdir(PATH + '/trajectories/' + GAME_NAME):
@@ -153,7 +158,7 @@ def build_cnn_model(nb_action):
     model.add(Flatten())
     model.add(Dense(512, activation="relu"))
     model.add(Dense(nb_action, activation="softmax"))
-    
+
     return model
 
 def tarin(model, nb_action, preprocess=True):
@@ -166,15 +171,22 @@ def tarin(model, nb_action, preprocess=True):
         status, action = load_traj_prepro(nb_action, USE_TRAJ_RATIO)
 
         print('-----------------------------------------------------')
+        # 軌跡データ保存
+        dirname = 'data_' + str(USE_TRAJ_RATIO)
+
+        if os.path.exists(dirname) == False:
+            os.mkdir(dirname)
         print('Now Save Numpy')
-        np.save('data/status.npy', status)
-        np.save('data/action.npy', action)
+        np.save(dirname + '/status.npy', status)
+        np.save(dirname + '/action.npy', action)
         print('End Save Numpy')
     else:
         # 保存済みデータ使用
+        dirname = 'data_' + str(USE_TRAJ_RATIO)
+
         print('Now Load Numpy')
-        status = np.load('data/status.npy')
-        action = np.load('data/action.npy')
+        status = np.load(dirname + '/status.npy')
+        action = np.load(dirname + '/action.npy')
         print('End Load Numpy')
 
     # モデルのコンパイル
@@ -185,11 +197,19 @@ def tarin(model, nb_action, preprocess=True):
     # テンソルボード
     tb = TensorBoard(log_dir="./logs")
 
+    # ラベルの重み付け
+    weight = None
+    if USE_CLASS_WEIGHT:
+        unique, count = np.unique(action, return_counts=True)
+        weight = np.max(count) / count
+        weight = dict(zip(unique, weight))
+
     # 学習
     history = model.fit(status, 
-                        np_utils.to_categorical(action, nb_action),                  
+                        np_utils.to_categorical(action, nb_action),
                         batch_size=BATCH_SIZE,                       
-                        epochs=EPOCHS,                                
+                        epochs=EPOCHS,
+                        class_weight=weight,
                         callbacks=[tb])
 
 def test(model, env):
@@ -229,8 +249,6 @@ def test(model, env):
         # 行動選択
         action = model.predict_on_batch(np.array([state]))
 
-        #print(np.argmax(action))
-
         # 行動
         observation, _, done, _ = env.step(np.argmax(action))
             
@@ -244,13 +262,13 @@ def main():
     env = gym.make(ENV_NAME)
 
     # 動画保存
-    env = wrappers.Monitor(env, './movie_folder_' + str(USE_TRAJ_RATIO) + '_' + str(EPOCHS), video_callable=(lambda ep: True), force=True)
+    env = wrappers.Monitor(env, './movie_folder_' + str(USE_TRAJ_RATIO) + '_' + str(EPOCHS) +'_' + str(BATCH_SIZE) + ('_CW' if USE_CLASS_WEIGHT else ''), video_callable=(lambda ep: True), force=True)
     
     # モデル構築
     model = build_cnn_model(env.action_space.n)
 
     # モデル学習
-    tarin(model, env.action_space.n, RAN_PREPROCESS)
+    tarin(model, env.action_space.n, RUN_PREPROCESS)
 
     # テスト
     test(model, env)
@@ -259,6 +277,22 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('--epoch', type=int, default=15)
+    parser.add_argument('-b', '--batchsize', type=int, default=128)
+    parser.add_argument('-p', '--preprocess', action="store_true")
+    parser.add_argument('-cw', '--classweight', action="store_true")
+    parser.add_argument('--raito', type=float, default=0.01)
+    args = parser.parse_args()
+
+    PATH = args.path
+    EPOCHS = args.epoch
+    BATCH_SIZE = args.batchsize
+    RUN_PREPROCESS = args.preprocess
+    USE_CLASS_WEIGHT = args.classweight
+    USE_TRAJ_RATIO = args.raito
+
     #実行時間計測
     start_time = time.time()
     
